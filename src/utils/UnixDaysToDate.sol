2d7dd9f0086259cd22341ba379d93fed4eec1011
mattg-a16z <mgleason@a16z.com>
2023-03-13 11:34:43 -0700

    corrected some more comments

--- src/utils/UnixDaysToDate.sol
+++ src/utils/UnixDaysToDate.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 contract UnixDaysToDate {
 
     /**
-     * Uses Fliegel and van Flandern algorithm for converting from timestamp to date
+     * Uses Fliegel and van Flandern algorithm for converting from days to date
      */
     function _daysToDate(uint64 _days) internal pure returns(uint256 _year, uint256 _month, uint256 _day) {
         unchecked {
-            uint256 L = _days + 68569 + 2440588; // Convert to march 1 4800 BCE
+            uint256 L = _days + 68569 + 2440588; // Convert to march 1 4900 BCE
             uint256 N = (4 * L) / 146097; // 4 * number of 400 year cycles
             L = L - (146097 * N + 3) / 4; // subtract out the 400 year cycles (rounding up)
             _year = (4000 * (L+1)) / 1461001; // number of leap years (days / 1461.001)
@@ -17,7 +17,7 @@ contract UnixDaysToDate {
             _day = L - (2447 * _month) / 80; // Remove months to get remaining days
             L = _month / 11; // Check if the month wraps around (11 is Jan, 12 is Feb)
             _month = _month + 2 - 12 * L; // translate month into real month
-            _year = 100 * (N - 49) + _year + L; // translate from 4800 BCE frame to CE frame
+            _year = 100 * (N - 49) + _year + L; // translate from 4900 BCE frame to CE frame
         }
     }
 }
